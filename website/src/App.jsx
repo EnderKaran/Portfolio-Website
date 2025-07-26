b495c374ff7593da2e97cc37b5c76b78383b6ee6
Ender <ender.karan14@gmail.com>
2025-07-26 16:45:02 +0300

    Hero sayfası bitti ve About sayfası başlanıldı

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import { About } from "./pages/About"
+import { Hero } from "./pages/Hero"
 
 function App() {
   
 
   return (
     <>
-      
+      <Hero />
+      <About/>
     </>
   )
 }
---
+++ website/src/pages/About.jsx
@@ -0,0 +1,27 @@
+export const About = () => {
+    return (
+        <section id="about" className="text-white p-8">
+            <h2 className="text-6xl font-bold mb-8">
+                Hakkım<span className="text-emerald-300">da</span>
+            </h2>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                <div className="border border-white/20 rounded-lg p-6">
+                    <h3 className="text-2xl font-bold mb-2">01. Arkaplan</h3>
+                    <p className="text-white/50 mb-6">
+                        Bilgisayar bilimlerinde güçlü bir temele ve yenilikçi web çözümleri yaratmaya tutkuyla bağlı, tutkulu bir Front-end geliştiriciyim.
+                        Teknoloji yolculuğum, işlerin nasıl yürüdüğüne dair merakımla başladı ve bu da beni web geliştirme alanında bir kariyer yapmaya yöneltti.
+                    </p>
+
+                    <div className="rounded-lg p-4 mb-4 border border-white/20">
+
+                    </div>
+
+                </div>
+
+               
+
+            </div>
+        </section>
+    )
+}
