4b4deee0afa322f7923bfc3f4a79f90d1eb8bf01
EnderKaran <ender.karan14@gmail.com>
2026-03-10 12:09:22 +0300

    sanity kurulumunu yaptim

--- portfolio-next/app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import { AboutPageContent } from '@/src/components/AboutPageContent';
-
-export default function AboutPage() {
-  return <AboutPageContent />;
-}
--- portfolio-next/app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import { Contact } from '@/src/components/Contact';
-
-export default function ContactPage() {
-  return <Contact />;
-}
